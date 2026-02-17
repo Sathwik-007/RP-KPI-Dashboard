21e27a9b528848c4e4de65f82b35b8afbbe1ac76
002-mash <002.lvmash@gmail.com>
2026-02-17 14:44:25 +0530

    Added another user

--- src/seed.js
+++ src/seed.js
@@ -4,13 +4,21 @@ const seed = async () => {
   await db.sequelize.sync();
 
   // Create a User (so you can login)
-  const user = await db.User.create({
+  await db.User.create({
     email: "admin@test.com",
     password: "password123",
     organizationId: 1
   });
 
+  await db.User.create({
+    email: "user@test.com",
+    password: "password12345",
+    organizationId: 2
+  });
+
   console.log("✅ User created: admin@test.com / password123");
+  console.log("✅ User created: user@test.com / password12345");
+  
 
   // Create Dummy Tickets
   const tickets = [
@@ -19,12 +27,14 @@ const seed = async () => {
     { title: "Setup Database", status: "Done", priority: "Critical", spaceOrProject: "Backend", organizationId: 1 },
     { title: "API Documentation", status: "Open", priority: "Low", spaceOrProject: "Docs", organizationId: 1 },
     { title: "Unit Testing", status: "Done", priority: "High", spaceOrProject: "QA", organizationId: 1 },
+    { title: "API Documentation", status: "Open", priority: "Low", spaceOrProject: "Docs", organizationId: 2 },
+    { title: "Unit Testing", status: "Done", priority: "High", spaceOrProject: "QA", organizationId: 2 },
     // Overdue Ticket (Due date in the past)
     { title: "Legacy Code Cleanup", status: "Open", priority: "Low", spaceOrProject: "Backend", organizationId: 1, dueDate: new Date("2023-01-01") }
   ];
 
   await db.Ticket.bulkCreate(tickets);
-  console.log("✅ 6 Dummy Tickets created!");
+  console.log(`✅ ${tickets.length} Dummy Tickets created!`);
 };
 
 seed().then(() => process.exit());
